Added Extra Grid Search Setups and Fixed W_in scaling (divided by 2) in Grid Search

Control_Panel.py
@@ -18,12 +18,12 @@ np.random.seed(2020)
 # Run Parameters
 #=======================================================================================================================
 system_name = "Colpitts"
-run_system = True # Generate new data from chosen system
+run_system = False # Generate new data from chosen system
 N_x = 2000 # Number of nodes in reservoir."should be at least equal to the estimate of independent real values
 # the reservoir has to remember from the input to solve its task"
 # -Lukosevicius in PracticalESN
 perform_grid_search = True
-setup_number = 1
+setup_number = 4
 sparsity_tuples = np.array([[5/N_x,1.0]
                             ])
 preload_W = True
@@ -74,6 +74,8 @@ print("Shape of state_target array: "+str(np.shape(state_target)))
 num_timesteps_data = np.shape(state_target)[1]
 
 state_target = np.divide(state_target,np.max(np.abs(state_target))) # Actual normalized input to reservoir.
+print("Max of target array")
+print(np.max(np.abs(state_target)))
 state = np.empty((N_y, train_end_timestep+timesteps_for_prediction)) # Input to reservoir. Before train_end_timestep,
 # state is identical to state_target. After that index, it will differ as this is a prediction of the state by the
 # reservoir.

Grid_Search_Settings.py
@@ -4,12 +4,39 @@ import numpy as np
 def Set_Grid(state_target, perform_grid_search, setup_number):
     # For parameter search by grid search:
     if setup_number == 1:
-        # "Broad Grid Search"
-        extra_W_in_scale_factor_grid = np.linspace(0.5, 2.0, 5) # input scalings grid, makes no difference for L96?
+        # "Broad Grid Search for General System Investigation"
+        extra_W_in_scale_factor_grid = np.linspace(0.25, 1.0, 5) # input scalings grid, makes no difference for L96?
         scaling_W_grid = np.linspace(0.3, 1.3, 5) # direct multiplier for spectral radius grid after normalization occurs
         alpha_grid = np.linspace(0.1, 1.0, 9) # uniform leaking rate grid
         # Secondary parameters to grid search
         beta_grid = np.logspace(-4, 2, 6)
+        extra_W_fb_scale_factor_grid = np.array(range(1,2))/1.0 # input scalings grid
+
+    if setup_number == 2:
+        # "L63 Low Spectral Radius Specific Grid Search"
+        extra_W_in_scale_factor_grid = np.linspace(0.25, 1.0, 5) # input scalings grid, makes no difference for L96?
+        scaling_W_grid = np.linspace(0.3, 0.7, 5) # direct multiplier for spectral radius grid after normalization occurs
+        alpha_grid = np.linspace(0.6, 0.95, 9) # uniform leaking rate grid
+        # Secondary parameters to grid search
+        beta_grid = np.logspace(-5, -3, 6)
+        extra_W_fb_scale_factor_grid = np.array(range(1,2))/1.0 # input scalings grid
+
+    if setup_number == 3:
+        # "L63 High Spectral Radius Specific Grid Search"
+        extra_W_in_scale_factor_grid = np.linspace(1.0, 1.7, 5) # input scalings grid, makes no difference for L96?
+        scaling_W_grid = np.linspace(1.0, 1.5, 5) # direct multiplier for spectral radius grid after normalization occurs
+        alpha_grid = np.linspace(0.6, 0.95, 9) # uniform leaking rate grid
+        # Secondary parameters to grid search
+        beta_grid = np.logspace(-5, -3, 6)
+        extra_W_fb_scale_factor_grid = np.array(range(1,2))/1.0 # input scalings grid
+
+    if setup_number == 4:
+        # "Colpitts-Specific Grid Search"
+        extra_W_in_scale_factor_grid = np.linspace(0.1, 0.7, 5) # input scalings grid, makes no difference for L96?
+        scaling_W_grid = np.linspace(0.5, 1.3, 5) # direct multiplier for spectral radius grid after normalization occurs
+        alpha_grid = np.linspace(0.5, 0.9, 9) # uniform leaking rate grid
+        # Secondary parameters to grid search
+        beta_grid = np.logspace(-3, -1, 6)
         extra_W_fb_scale_factor_grid = np.array(range(1,2))/1.0 # input scalings grid
 
     if perform_grid_search:
@@ -18,14 +45,14 @@ def Set_Grid(state_target, perform_grid_search, setup_number):
         list_of_scaling_alpha = alpha_grid
         list_of_beta_to_test = beta_grid
         list_of_scaling_W_fb = extra_W_fb_scale_factor_grid
-        list_of_scaling_W_in = list_of_W_in_scale_factor * np.max(state_target)
+        list_of_scaling_W_in = list_of_W_in_scale_factor * np.max(np.abs(state_target))
     else:
         list_of_W_in_scale_factor = [0.5]
         list_of_scaling_W = [0.175]
         list_of_scaling_alpha = [1.0]
         list_of_beta_to_test = [0.001]
         list_of_scaling_W_fb = [1.0]
-        list_of_scaling_W_in = np.array(list_of_W_in_scale_factor) * np.max(state_target)
+        list_of_scaling_W_in = np.array(list_of_W_in_scale_factor) * np.max(np.abs(state_target))
 
     return list_of_scaling_W, list_of_scaling_alpha, list_of_beta_to_test, \
            list_of_scaling_W_fb, list_of_scaling_W_in